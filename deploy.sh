50e690b927e82c6f809f28f0f95aa9df83a94c9b
pulivemareddy <134262776+pulivemareddy@users.noreply.github.com>
2026-01-31 15:49:23 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 set -e
 
+# Update system
+sudo dnf update -y
+
+# Install Docker if not installed
+if ! command -v docker &> /dev/null
+then
+  sudo dnf install docker -y
+fi
+
+# Enable & start Docker
+sudo systemctl enable docker
+sudo systemctl start docker
+
+# Add ec2-user to docker group
+sudo usermod -aG docker ec2-user
+
+# Pull latest image
 REGION=eu-north-1
 ACCOUNT_ID=124355642518
 REPO=aws-ci-cd-project
 
-sudo systemctl start docker
-sudo systemctl enable docker
-
-aws ecr get-login-password --region $REGION \
-| docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
-
-docker stop flask-app || true
-docker rm flask-app || true
+aws ecr get-login-password --region $REGION | \
+docker login --username AWS --password-stdin \
+$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 
 docker pull $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO:latest
 
+# Stop old container if exists
+docker stop flask-app || true
+docker rm flask-app || true
+
+# Run container
 docker run -d -p 80:5000 --name flask-app \
 $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO:latest
